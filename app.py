0e2eb895ed78ded55c9e140af92cd35c59293fbc
Jozef Dochan <theglow666@gmail.com>
2017-10-03 15:35:15 +0200

    added support for sending notification after completion of download

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
+from __future__ import print_function
 import threading
 from functools import wraps
-
+import sys
 import os
+
+import sys
 from flask import Flask, request, jsonify, Response
 from pip._vendor import requests
-import youtube_dl
+from pushbullet import InvalidKeyError
 
 app = Flask(__name__)
 PATH_TO_SAVE = "{PATH}"
 user = '{UserName}'
 user_pass = '{Password}'
-ydl = youtube_dl.YoutubeDL({"outtmpl": PATH_TO_SAVE + "%(title)s.%(ext)s"})
+notify_via_pushbullet = True
+pushbullet_token = "{TOKEN}"
+
+try:
+    import youtube_dl
+
+    ydl = youtube_dl.YoutubeDL({"outtmpl": PATH_TO_SAVE + "%(title)s.%(ext)s"})
+except ImportError:
+    ydl = None
 
 
 def check_auth(username, password):
@@ -49,6 +60,18 @@ def hello_world():
         return jsonify(result), 400
 
 
+def on_complete(filename):
+    if notify_via_pushbullet:
+        try:
+            from pushbullet import Pushbullet
+            pb = Pushbullet(pushbullet_token)
+            pb.push_note("Download finished", filename)
+        except ImportError:
+            print("Cant notify via PushBullet. Missing librabry. Install via pip install pushbullet.py", file=sys.stderr)
+        except InvalidKeyError:
+            print("Invalid Api key for PushBullet. Please insert correct one.", file=sys.stderr)
+
+
 def download_in_background(data):
     url = data["url"]
 
@@ -66,19 +89,25 @@ def download_in_background(data):
         path = PATH_TO_SAVE + name
 
     if "youtube" in url or "oload" in url:
-        with ydl:
-            ydl.download([url])
+        if ydl is not None:
+            with ydl:
+                info = ydl.extract_info(url=url, download=True)
+                on_complete(info["title"])
 
     else:
         r = requests.get(url)
         with open(path, "wb+") as code:
             code.write(r.content)
+        on_complete(name)
 
 
 def url_check(url):
     req = requests.get(url)
+    if "youtube" in url or "oload" in url:
+        if ydl is None:
+            raise Exception("Trying to download from Youtube/Openload without youtube-dl library")
     return req.status_code == 200
 
 
 if __name__ == '__main__':
-    app.run(port=9000)
+    app.run(port=9000, debug=True)
