af7a51a612204efddaf458d0a772d6855bde339c
Jozef Dochan <theglow666@gmail.com>
2017-10-04 10:49:21 +0200

    now loading settings from configuration file

--- app.py
+++ app.py
@@ -2,29 +2,35 @@ from __future__ import print_function
 import threading
 from functools import wraps
 import os
-
+import yaml
 import sys
 from flask import Flask, request, jsonify, Response
 from pip._vendor import requests
-from pushbullet import InvalidKeyError
 
 app = Flask(__name__)
-PATH_TO_SAVE = "{PATH}"
-user = '{UserName}'
-user_pass = '{Password}'
-notify_via_pushbullet = True
-pushbullet_token = "{TOKEN}"
+
+
+def load_conf():
+    directory = os.path.dirname(os.path.abspath(__file__))
+    with open(directory+"\config.yml", 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as e:
+            print(e, file=sys.stderr)
+
+
+conf = load_conf()
 
 try:
     import youtube_dl
 
-    ydl = youtube_dl.YoutubeDL({"outtmpl": PATH_TO_SAVE + "%(title)s.%(ext)s"})
+    ydl = youtube_dl.YoutubeDL({"outtmpl": conf["path"] + "%(title)s.%(ext)s"})
 except ImportError:
     ydl = None
 
 
 def check_auth(username, password):
-    return username == user and password == user_pass
+    return username == conf["username"] and password == conf["password"]
 
 
 def authenticate():
@@ -50,25 +56,33 @@ def requires_auth(f):
 def hello_world():
     try:
         data = request.get_json()
+
+        if data is None:
+            raise Exception("Missing or wrong Content-Type, should be: application/json")
+
         if url_check(data["url"]):
             threading.Thread(target=lambda: download_in_background(data)).start()
             result = {"success": True}
             return jsonify(result)
     except Exception as e:
         result = {"error": True, "reason": str(e)}
-        return jsonify(result), 400
+        return jsonify(result), 500
 
 
 def on_complete(filename):
-    if notify_via_pushbullet:
+    if conf["notify_via_pushbullet"]:
         try:
             from pushbullet import Pushbullet
-            pb = Pushbullet(pushbullet_token)
+            pb = Pushbullet(conf["pushbullet_token"])
             pb.push_note("Download finished", filename)
+            return True
         except ImportError:
-            print("Cant notify via PushBullet. Missing librabry. Install via pip install pushbullet.py", file=sys.stderr)
-        except InvalidKeyError:
+            print("Cant notify via PushBullet. Missing librabry. Install via pip install pushbullet.py",
+                  file=sys.stderr)
+            return False
+        except Exception:
             print("Invalid Api key for PushBullet. Please insert correct one.", file=sys.stderr)
+            return False
 
 
 def download_in_background(data):
@@ -81,11 +95,11 @@ def download_in_background(data):
         name = url.split("/")[-1]
 
     if "category" in data:
-        path = PATH_TO_SAVE + data["category"] + "/" + name
+        path = conf["path"] + data["category"] + "/" + name
         if not os.path.exists(path):
             os.makedirs(os.path.dirname(path))
     else:
-        path = PATH_TO_SAVE + name
+        path = conf["path"] + name
 
     if "youtube" in url or "oload" in url:
         if ydl is not None:
